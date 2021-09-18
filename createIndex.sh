6205e4cf4f7d1471b4afa91da4c7b8d325a98dd9
Jens Walter <jens@apimeister.com>
2021-09-18 22:03:15 +0200

    update

--- createIndex.sh
+++ createIndex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f ! -name '.*' | grep -v '.git' | grep -v 'Icon' | grep -v 'index.txt' | grep -v '.md' | grep -v 'createIndex.sh' > index.txt
+find . -type f ! -name '.*' | grep -v '.git' | grep -v 'Icon' | grep -v 'index.txt' | grep -v '.md' | grep -v 'createIndex.sh' |sort > index.txt
